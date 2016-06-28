c731015e332c3a49ccd594aa7e627d4647786632
tmugrage <tmugrage@live.com>
2016-06-28 17:09:36 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  include users
   notify { "Hello, my name is ${::hostname}": }
 
 file { '/etc/motd':
@@ -52,5 +53,3 @@ file { '/etc/motd':
  content => "Think before you type\n this is a new line\n",
 }
 }
-
-include users
