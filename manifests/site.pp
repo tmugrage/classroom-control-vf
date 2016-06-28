a74345b6c5d550a023e510d1f1c5fe0b67487c08
tmugrage <tmugrage@live.com>
2016-06-28 14:18:21 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,4 +45,10 @@ node default {
   notify { "Hello, my name is ${::hostname}": }
 }
 
-notify { "Hello, my name is ${::hostname}": }
+file { '/etc/motd':
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0777',
+  content => 'This is a long class',
+}
