848980d30ba30f8d3d13c084cb7483cc12b5c97a
tmugrage <tmugrage@live.com>
2016-06-29 16:41:23 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,6 +46,12 @@ node default {
   include skeleton
   include memcached
   include nginx
+ 
+    if $::virtual != 'physical' {
+        # enter puppet code
+        $vmname = capitalize($::virtual)
+        notify { 'This is a ${vmname} virtual machine.': } 
+    }
   # notify { "Hello, my name is ${::hostname}": }
 
 file { '/etc/motd':
