3cba61c9bb711e142c6dc7cab0ff735c391da764
tmugrage <tmugrage@live.com>
2016-06-29 12:41:37 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,1 +1,20 @@
-
+# Class: memcached
+#
+#
+class memcached {
+  # resources
+  package { 'memcached':
+    ensure => 'installed',
+  }
+  service { 'memcached':
+    enable => true,
+    ensure => running,
+}
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => 0644,
+    content => 'PORT="11211"\n\rUSER="memcached"\n\rMAXCONN="96"\n\rCACHESIZE="32"\n\rOPTIONS= ""\n\r'
+  }
+}
