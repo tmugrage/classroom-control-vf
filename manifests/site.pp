010ccef958a88f2b8902661298be883dbce7587e
Timothy Mugrage <tmugrage@cisco.com>
2018-05-04 09:51:08 -0400

    blah

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,62 @@
 ## site.pp ##
 
 
+## Active Configurations ##
+
+# PRIMARY FILEBUCKET
+# This configures puppet agent and puppet inspect to back up file contents when
+# they run. The Puppet Enterprise console needs this to display file contents
+# and differences.
+
+# Disable filebucket by default for all File resources:
+File { backup => false }
+
+# Randomize enforcement order to help understand relationships
+ini_setting { 'random ordering':
+  ensure  => present,
+  path    => "${settings::confdir}/puppet.conf",
+  section => 'agent',
+  setting => 'ordering',
+  value   => 'title-hash',
+}
+
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
+
+# The default node definition matches any node lacking a more specific node
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
+# new comment line
+
+node default {
+  # This is where you can declare classes for all nodes.
+  # Example:
+  #   class { 'my_class': }
+  include users
+  include skeleton
+  include memcached
+  include nginx
+  include aliases
+  
+  #nginx::vhost { 'droidmagic.com': }
+  
+    if $::is_virtual {
+        $vmname = capitalize($::virtual)
+        notify { "This is a ${vmname} virtual machine.": } 
+    }
+  # notify { "Hello, my name is ${::hostname}": }
+
+$message = hiera('message')
+notify { $message: }
+
+file { '/etc/motd':
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0777',
+ content => "Think before you type\n this is a new line\n",
 }
 }
