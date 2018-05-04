7122a12b8c1eb71f20879128f273644fc71c7fb4
Timothy Mugrage <tmugrage@cisco.com>
2018-05-04 09:29:23 -0400

    multi file change

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,12 +33,6 @@ ini_setting { 'random ordering':
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
 # node definitions.
 
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-# new comment line
-
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
